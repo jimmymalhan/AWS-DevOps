72b262a731269c5e51b708da21710666b31dea68
Jimmy Malhan <jimmymalhan999@gmail.com>
2021-05-02 17:43:02 -0700

    updating with custom provider

--- terraform/Project 2/ec2.tf
+++ terraform/Project 2/ec2.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-001628438d5d7d524"
   instance_type = "t3.micro"
